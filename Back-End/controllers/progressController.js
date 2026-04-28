1b9702e49e1c0ce92bc19a3ec7f7a2aca4b2bf00
ZhalgasB <zhalgas.bissenbai@narxoz.kz>
2026-04-29 00:34:03 +0500

    progress Controller

---
+++ Back-End/controllers/progressController.js
@@ -0,0 +1,54 @@
+const LessonModel = require('../models/lessonModel');
+const ProgressModel = require('../models/progressModel');
+const ExerciseModel = require('../models/exerciseModel');
+const AttemptModel = require('../models/attemptModel');
+const StreakModel = require('../models/streakModel');
+const UserBadgeModel = require('../models/userBadgeModel');
+
+exports.getChildProgress = async (req, res) => {
+  try {
+    const { childId } = req.params;
+    const lessons = await LessonModel.findAll();
+
+    const result = await Promise.all(lessons.map(async (lesson) => {
+      const progress = await ProgressModel.findByChildAndLesson(childId, lesson.id) || 
+                       { completed: false, score: 0 };
+
+      const exercises = await ExerciseModel.findByLessonId(lesson.id);
+      const attempts = await AttemptModel.getLastCorrectPerExercise(childId, lesson.id);
+      const completedExercises = attempts.filter(a => a.last_correct === true).length;
+
+      return {
+        ...lesson,
+        progress,
+        totalExercises: exercises.length,
+        completedExercises,
+        completionRate: progress.score || 0
+      };
+    }));
+
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getChildDashboard = async (req, res) => {
+  try {
+    const { childId } = req.params;
+    const totalXp = await AttemptModel.sumXpByChild(childId);
+    const streak = await StreakModel.findByChild(childId) || { current_streak: 0, longest_streak: 0 };
+    const lessonsCompleted = await require('../services/gamificationService').countCompletedLessons(childId);
+
+
+
+    res.json({
+      totalXp,
+      currentStreak: streak.current_streak,
+      longestStreak: streak.longest_streak,
+      lessonsCompleted
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
