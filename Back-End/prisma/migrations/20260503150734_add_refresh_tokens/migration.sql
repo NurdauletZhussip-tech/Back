09852f6ebee1e7e2324ebc37691e670f52322265
NurdauletZhussip-tech <nurdaulet.zhussip.studying@gmail.com>
2026-05-03 21:36:38 +0500

    Жалгас кешрш оказывается я просто забыл вот это закомитить

---
+++ Back-End/prisma/migrations/20260503150734_add_refresh_tokens/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "refresh_tokens" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "user_id" UUID NOT NULL,
+    "token" VARCHAR(255) NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "refresh_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "refresh_tokens_token_key" ON "refresh_tokens"("token");
+
+-- CreateIndex
+CREATE INDEX "idx_refresh_user" ON "refresh_tokens"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
